94600e9d2b5e226aac3e7bfc67e989f6f8717954
achmad <achmadalfazari@gmail.com>
2022-09-23 06:38:34 +0700

    add project folder

---
+++ projects/simple/server/server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "net/http"
+
+    "github.com/labstack/echo"
+)
+
+type HelloWorld struct {
+    Message string `json:"message"`
+}
+
+func main() {
+    e := echo.New()
+    e.GET("/", Greetings)
+    e.Logger.Fatal(e.Start(":3000"))
+}
+
+func Greetings(c echo.Context) error {
+    return c.JSON(http.StatusOK, HelloWorld{
+        Message: "Hello World",
+    })
+}
