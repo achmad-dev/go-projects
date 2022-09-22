169b4cb5270a9edf2c55d386f69a8835f000b560
achmad <achmadalfazari@gmail.com>
2022-09-22 23:53:20 +0700

    update on slice

--- intro/slice/main.go
+++ intro/slice/main.go
@@ -1,10 +1,15 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 var a int
 var b[] int
 func main() {
 	c := []int{}
+	d := []string{"human earth", "human mars"}
+	species := []string{}
 	a = 12
 	b = append(b, 1)
 	c = append(c, 3)
@@ -12,4 +17,10 @@ func main() {
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
+	for index, race := range d {
+		var specie = strings.Fields(race)
+		species = append(species, specie[0])
+		fmt.Println(index, "index")
+	}
+	fmt.Println(species)
 }
